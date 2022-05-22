de4392afc7e33029cb21a44fb7911e2ef5eacf28
gabrielliraaraujo <51405289+gabrielliraaraujo@users.noreply.github.com>
2022-05-22 08:49:56 -0300

    AddingErrorBack

--- app.py
+++ app.py
@@ -10,5 +10,14 @@ def pagina_inicial():
     today = "Good Morning, today is" + str(date.today())
     return today
 
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+
 if __name__ == '__main__':
     app.run()
